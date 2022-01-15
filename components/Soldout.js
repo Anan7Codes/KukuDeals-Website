ad64a1db97df407979eca026360bcf8fbbc18e88
Anan7Codes <anan07dhu@outlook.com>
2022-01-15 11:43:05 +0400

    Arrow Positioning and Key fixed

--- components/Soldout.js
+++ components/Soldout.js
@@ -7,30 +7,31 @@ import ArrowR from "./ArrowR";
 const responsive = {
   0: { items: 1 },
   1024: { items: 4 },
+  1736: { items: 5 }
 };
 const items = [
-  <div className="item" data-value="0">
+  <div className="item" data-value="0" key="0">
     <Soldout1 />
   </div>,
-  <div className="item" data-value="1">
+  <div className="item" data-value="1" key="1">
     <Soldout1 />
   </div>,
-  <div className="item" data-value="2">
+  <div className="item" data-value="2" key="2">
     <Soldout1 />
   </div>,
-  <div className="item" data-value="3">
+  <div className="item" data-value="3" key="3">
     <Soldout1 />
   </div>,
-  <div className="item" data-value="4">
+  <div className="item" data-value="4" key="4">
     <Soldout1 />
   </div>,
-  <div className="item" data-value="5">
+  <div className="item" data-value="5" key="5">
     <Soldout1 />
   </div>,
-  <div className="item" data-value="6">
+  <div className="item" data-value="6" key="6">
     <Soldout1 />
   </div>,
-  <div className="item" data-value="7">
+  <div className="item" data-value="7" key="7">
     <Soldout1 />
   </div>,
 ];
@@ -39,8 +40,8 @@ export default function Soldout() {
   return (
     <div>
       <div className="">
-        <div className="bg-[#f53435] h-[35rem]  rounded-[45px] px-6 py-5  text-white">
-          <div className="p-4 ml-3">
+        <div className="bg-[#f53435] h-[35rem] rounded-[45px] px-6 py-5 text-white">
+          <div className="p-4 ml-3 relative">
             <div className="flex justify-between">
               <p className="font-[1000] text-4xl">Sold Out</p>
             </div>
@@ -49,13 +50,13 @@ export default function Soldout() {
               All our sold out campaigns along with their <br /> corresponding
               draw dates are listed below
             </div>
-            <div className="">
+            <div className="mt-8">
               <AliceCarousel
                 mouseTracking
                 items={items}
                 responsive={responsive}
-                renderPrevButton={( )=>{return <ArrowL/>}}
-                renderNextButton={( )=>{return <ArrowR/>}}
+                renderPrevButton={( )=>{return (<div className="absolute -top-24 right-24"><ArrowL/></div>)}}
+                renderNextButton={( )=>{return (<div className="absolute -top-24 right-20"><ArrowR/></div>)}}
                 disableDotsControls="true"
                 controlsStrategy="alternate"
               />

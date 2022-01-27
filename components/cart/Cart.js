fa80332ddb25d3bc821d56e5756fcfd6d8735a0c
MohammedHafiz <mohammedhafizba@gmail.com>
2022-01-27 23:55:05 +0530

    responsive issues fixed

--- components/cart/Cart.js
+++ components/cart/Cart.js
@@ -8,7 +8,7 @@ export default function Cart() {
       <div className="col-span-9 lg:w-[95%]">
         <p className="text-4xl font-bold text-gray-700">Cart</p>
         <div className="lg:hidden"></div>
-        <div className="bg-white flex justify-center items-center lg:justify-start h-44 rounded-t-3xl my-6">
+        <div className="bg-white flex  items-center lg:justify-start h-44 rounded-t-3xl my-6">
           <div className=" bg-white cursor-pointer rounded-[15px] object-fit -left-2 relative ml-10 mt-2 border border-gray-200 w-32 h-28">
             <Image
               src="/icons/products/product.png"
@@ -16,18 +16,24 @@ export default function Cart() {
               alt="product logo"
             />
           </div>
-          <div className="flex pl-1 pt-2 pr-6">
-            <div>
-              <p className="text-xl font-bold">2 DXB license plates</p>
-              <p className="text-xl leading-3">Balnco Set</p>
-              <p className="font-bold text-xl pt-3 text-blue-500">AED30.00</p>
-              <p className="text-xs font-semibold pt-3 text-green-500">
+          <div className="flex pl-1 pt-2 	">
+            <div className="">
+              <p className=" text-sm sm:text-base lg:text-xl font-bold leading-2">
+                2 DXB license plates
+              </p>
+              <p className="text-sm sm:text-base lg:text-xl pt-1 leading-2 lg:leading-3">
+                Balnco Set
+              </p>
+              <p className="font-bold text-sm sm:text-base lg:text-xl lg:pt-3 text-blue-500">
+                AED30.00
+              </p>
+              <p className="text-xs font-semibold lg:pt-3 text-green-500">
                 2 Coupons
                 <span className="text-black font-normal"> per unit</span>
               </p>
             </div>
 
-            <div className=" sm:flex lg:flex ml-28 pt-3 leading-extra-loose">
+            <div className=" sm:flex lg:flex pt-3 sm:ml-12 lg:ml-16 leading-extra-loose">
               <div className="hidden sm:flex lg:flex">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -47,7 +53,7 @@ export default function Cart() {
                   1
                 </div>
               </div>
-              <div className=" sm:hidden lg:hidden">
+              <div className=" sm:hidden lg:hidden pl-3 pr-4">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-6 w-6 bg-[#0073ff] text-white rounded-full cursor-pointer"
@@ -85,13 +91,13 @@ export default function Cart() {
           </div>
         </div>
         <div className="bg-[#5dd74b] text-right h-12 rounded-b-3xl">
-          <p className="text-white text-sm mr-3 pt-1">
+          <p className="text-white text-xs lg:text-sm mr-3 pt-1">
             Donate these product(s) to double the ticket(s)
             <ToggleSwitch sx={{ m: 1 }} defaultChecked />
           </p>
         </div>
 
-        <div className="bg-white flex justify-center items-center lg:justify-start h-44 rounded-t-3xl my-6">
+        <div className="bg-white flex  items-center lg:justify-start h-44 rounded-t-3xl my-6">
           <div className=" bg-white cursor-pointer rounded-[15px] object-fit -left-2 relative ml-10 mt-2 border border-gray-200 w-32 h-28">
             <Image
               src="/icons/products/product.png"
@@ -99,18 +105,24 @@ export default function Cart() {
               alt="product logo"
             />
           </div>
-          <div className="flex pl-1 pt-2 pr-6">
-            <div>
-              <p className="text-xl font-bold">2 DXB license plates</p>
-              <p className="text-xl leading-3">Balnco Set</p>
-              <p className="font-bold text-xl pt-3 text-blue-500">AED30.00</p>
-              <p className="text-xs font-semibold pt-3 text-green-500">
+          <div className="flex pl-1 pt-2 	">
+            <div className="">
+              <p className=" text-sm sm:text-base lg:text-xl font-bold leading-2">
+                2 DXB license plates
+              </p>
+              <p className="text-sm sm:text-base lg:text-xl pt-1 leading-2 lg:leading-3">
+                Balnco Set
+              </p>
+              <p className="font-bold text-sm sm:text-base lg:text-xl lg:pt-3 text-blue-500">
+                AED30.00
+              </p>
+              <p className="text-xs font-semibold lg:pt-3 text-green-500">
                 2 Coupons
                 <span className="text-black font-normal"> per unit</span>
               </p>
             </div>
 
-            <div className=" sm:flex lg:flex ml-28 pt-3 leading-extra-loose">
+            <div className=" sm:flex lg:flex pt-3 sm:ml-12 lg:ml-16 leading-extra-loose">
               <div className="hidden sm:flex lg:flex">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -125,13 +137,12 @@ export default function Cart() {
                     strokeWidth={1}
                     d="M12 10H6"
                   />
-                </svg>{" "}
+                </svg>
                 <div className="pt-2 pl-6 h-12 w-16 text-xl rounded-[15px] text-[#808080] bg-gray-200 mr-4">
                   1
                 </div>
               </div>
-
-              <div className="sm:hidden lg:hidden">
+              <div className=" sm:hidden lg:hidden pl-3 pr-4">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-6 w-6 bg-[#0073ff] text-white rounded-full cursor-pointer"
@@ -169,7 +180,7 @@ export default function Cart() {
           </div>
         </div>
         <div className="bg-[#5dd74b] text-right h-12 rounded-b-3xl">
-          <p className="text-white text-sm mr-3 pt-1">
+          <p className="text-white text-xs lg:text-sm mr-3 pt-1">
             Donate these product(s) to double the ticket(s)
             <ToggleSwitch sx={{ m: 1 }} defaultChecked />
           </p>

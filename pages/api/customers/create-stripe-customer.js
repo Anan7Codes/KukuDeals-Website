2028e0b9e4d8ee1d10ee5fa8561d9b2b29c1d92f
Anan7Codes <anand07dhu@outlook.com>
2022-02-10 17:03:26 +0400

    stripe customer api added

---
+++ pages/api/customers/create-stripe-customer.js
@@ -0,0 +1,26 @@
+import initStripe from 'stripe'
+import { supabaseAdmin } from "@/utils/supabaseClient";
+
+export default async function handler(req, res) {
+    if(req.method !== 'POST') {
+        return res.send({ success: false, message: 'Wrong request made'})
+    }
+    if(req.method === 'POST') {
+        if(req.query.API_ROUTE_SECRET !== process.env.RANDOM_CRYPTO_HASH) {
+            return res.status(401).send({ success: false, message: 'You are not authorized to make this call'})
+        }
+        console.log(req.body)
+        const stripe = initStripe(process.env.STRIPE_SECRET_KEY)
+        const customer = await stripe.customers.create({
+            email: req.body.record.email
+        })
+        console.log(customer)
+
+        const { data, error } = await supabaseAdmin
+            .from('profiles')
+            .update({ stripe_customer_id: customer.id })
+            .eq('id', req.body.record.id)
+        console.log(data, error)
+        return res.send({ message: `stripe customer created: ${customer.id}`})
+    }
+}

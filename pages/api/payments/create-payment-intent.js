be17a509944497facc171fe8becd8b7ebdb008c5
Anan7Codes <anand07dhu@outlook.com>
2022-02-13 21:01:15 +0400

    total payment check server size

--- pages/api/payments/create-payment-intent.js
+++ pages/api/payments/create-payment-intent.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 import Stripe from 'stripe'
+import { map } from 'modern-async'
 
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY, {apiVersion: '2020-08-27'})
 
@@ -7,19 +8,43 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseSecretKey = process.env.SUPABASE_SECRET_KEY
 const supabase = createClient(supabaseUrl, supabaseSecretKey)
 
+const TotalPrice = async (cart) => {
+    let total = 0
+    let success = true
+    await map(cart, async (item) => {
+        let { data, error } = await supabase
+        .from('campaigns')
+        .select('Price')
+        .eq("id", item.id)
+        if(error) return success = false
+        total += data[0].Price * item.qty
+    })
+    return { total, success }
+}
+
 export default async function handler(req, res) {
     if(req.method !== 'POST') {
         return res.send({ success: false, message: 'Wrong request made'})
     }
     if(req.method === 'POST') {
+        const { total, success } = await TotalPrice(req.body.cart)
+        if(!success) return res.status(404).json({ success: false, message: "Failed to calculate total amount"})
+
+        let { data, error } = await supabase
+            .from('profiles')
+            .select('stripe_customer_id')
+            .eq("id", req.body.user_id)
+        if(error) return res.status(404).json({ success: false, message: "Failed authorization"})
+
         const ephemeralKey = await stripe.ephemeralKeys.create(
-            {customer: "cus_L7s9dLjYRS2qbw"},
-            {apiVersion: '2020-08-27'}
+            { customer: data[0].stripe_customer_id },
+            { apiVersion: '2020-08-27' }
         );
+
         const paymentIntent = await stripe.paymentIntents.create({
-            amount: 1099,
+            amount: total * 100,
             currency: 'AED',
-            customer: 'cus_L7s9dLjYRS2qbw',
+            customer: data[0].stripe_customer_id,
             automatic_payment_methods: {
                 enabled: true,
             },
@@ -31,7 +56,7 @@ export default async function handler(req, res) {
         res.json({
             paymentIntent: paymentIntent.client_secret,
             ephemeralKey: ephemeralKey.secret,
-            customer: "cus_L7s9dLjYRS2qbw",
+            customer: data[0].stripe_customer_id,
         });
     }
 }

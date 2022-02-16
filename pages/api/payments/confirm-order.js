14e222798f2dd8a4dd955bbb4e236c42423c0130
Anan7Codes <anand07dhu@outlook.com>
2022-02-16 17:00:13 +0400

    debug over

--- pages/api/payments/confirm-order.js
+++ pages/api/payments/confirm-order.js
@@ -10,9 +10,7 @@ const supabaseSecretKey = process.env.SUPABASE_SECRET_KEY
 
 const supabase = createClient(supabaseUrl, supabaseSecretKey)
 
-const endpointSecret = "whsec_aNIcwFv9jP5EcTDKftddfNTP6AWAU6XW"
-const test = process.env.WEBHOOK_SECRET
-console.log("test ev", test)
+const endpointSecret = process.env.WEBHOOK_SECRET
 
 export const config = {
     api: {

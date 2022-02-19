e7a7ecfa04384f029ddbde6a873e8053fce71666
Anan7Codes <anand07dhu@outlook.com>
2022-02-19 20:28:38 +0400

    animations check & promo code check website

--- components/cart/CheckoutForm.js
+++ components/cart/CheckoutForm.js
@@ -47,8 +47,6 @@ export default function CheckoutForm() {
     e.preventDefault();
 
     if (!stripe || !elements) {
-      // Stripe.js has not yet loaded.
-      // Make sure to disable form submission until Stripe.js has loaded.
       return;
     }
 
@@ -62,11 +60,6 @@ export default function CheckoutForm() {
       },
     });
 
-    // This point will only be reached if there is an immediate error when
-    // confirming the payment. Otherwise, your customer will be redirected to
-    // your `return_url`. For some payment methods like iDEAL, your customer will
-    // be redirected to an intermediate site first to authorize the payment, then
-    // redirected to the `return_url`.
     if (error.type === "card_error" || error.type === "validation_error") {
       setMessage(error.message);
     } else {
@@ -78,14 +71,14 @@ export default function CheckoutForm() {
 
   return (
     <form id="payment-form" onSubmit={handleSubmit}>
-      <PaymentElement id="payment-element" />
-      <button disabled={isLoading || !stripe || !elements} id="submit">
-        <span id="button-text">
-          {isLoading ? <div className="spinner" id="spinner"></div> : "Pay now"}
+      <PaymentElement />
+      <button disabled={isLoading || !stripe || !elements} >
+        <span>
+          {isLoading ? <div></div> : "Pay now"}
         </span>
       </button>
       {/* Show any error or success messages */}
-      {message && <div id="payment-message">{message}</div>}
+      {message && <div>{message}</div>}
     </form>
   );
 }
--- components/cart/Payement.js
+++ components/cart/Payement.js
@@ -31,6 +31,7 @@ export default function Payement() {
     };
 
     const fetchPaymentSheetParams = async () => {
+      setLoading(true)
       try {
           const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/payments/create-payment-intent`, {
               method: 'POST',
@@ -45,6 +46,7 @@ export default function Payement() {
           });
           const { paymentIntent } = await response.json();
           setClientSecret(paymentIntent);
+          setLoading(false)
       } catch (e) {
           console.log("fetchparamserror", e)
       }
@@ -55,9 +57,25 @@ export default function Payement() {
       try {
           const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/promocodes/enter-promo-code`, {
               promoCode: promoCode,
-              user_id: user.id
+              user_id: userInfo.id
           })
           setPromoLoading(false)
+          if(!res.data.success) {
+            alert('Error', `${res.data.message}`)
+            setPromoCode('')
+            return
+          }
+
+          if(confirm(`${res.data.message}`) ==  true) {
+            setPromoCodeApplied(true)
+            if(res.data.data.type) {
+                setClientTotal(clientTotal - res.data.data.value)
+            } else {
+                setClientTotal(clientTotal - (clientTotal * res.data.data.value / 100))
+            }
+          } else {
+            setPromoCodeApplied(false)
+          }
       } catch (e) {
           console.log(e)
       }
@@ -81,37 +99,51 @@ export default function Payement() {
           <p className="pt-2 absolute pl-16 lg:pl-0 lg:mt-4 text-sm font-normal">
             (Inclusive of VAT)
           </p>
-          <p>AED{total}.00</p>
+          <p>AED{promoCodeApplied ? clientTotal.toFixed(2) : total.toFixed(2)}</p>
         </div>
         <div className="flex justify-between text-sm pb-3 pt-3">
           <p>Subtotal</p>
-          <p>AED{total*0.95}</p>
+          <p>AED{promoCodeApplied ? (clientTotal*0.95).toFixed(2) : (total*0.95).toFixed(2)}</p>
         </div>
         <div className="flex justify-between text-sm pb-3 pt-3">
           <p>VAT</p>
-          <p>AED{total*0.05}</p>
+          <p>AED{promoCodeApplied ? (clientTotal*0.05).toFixed(2) : (total*0.05).toFixed(2)}</p>
         </div>
       </div>
       <div>
         <div className="bg-white item-center justify-center rounded-2xl">
-          <input
-            placeholder="Promo Code"
-            className="border border-gray-400 pl-3 ml-5 my-4 outline-none text-xs rounded w-[70%] h-9"
-          />
-          <button className="ml-3 text-blue-500 border h-9 w-[16%] rounded text-xs font-semibold border-blue-500">
-            Apply
-          </button>
+          { promoCodeApplied ?
+            <>
+              <p>Promo code {promoCode} applied! Saving AED {(total - clientTotal).toFixed(2)}</p>
+              <button onClick={RemovePromoCode} className="ml-3 text-blue-500 border h-9 w-[16%] rounded text-xs font-semibold border-blue-500">
+                Remove
+              </button>
+            </>
+            :
+            <>
+              <input
+                placeholder="Promo Code"
+                className="border border-gray-400 pl-3 ml-5 my-4 outline-none text-xs rounded w-[70%] h-9"
+                value={promoCode}
+                onChange={e => setPromoCode(e.target.value)}
+              />
+              <button onClick={promoLoading ? null : EnterPromoCode} className="ml-3 text-blue-500 border h-9 w-[16%] rounded text-xs font-semibold border-blue-500">
+              { promoLoading ? <svg class="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24"></svg> : 'Apply' }
+              </button>
+            </>
+          }
         </div>
       </div>
       <div>
-        <button className="bg-blue-800 p-3 mt-4 w-full text-white font-bold h-16 rounded-[10px]" onClick={fetchPaymentSheetParams}>
-          Confirm Order
-        </button>
-        {clientSecret && (
+        {clientSecret ? 
           <Elements options={options} stripe={stripePromise}>
             <CheckoutForm />
           </Elements>
-        )}
+          :
+          <button className="bg-blue-800 p-3 mt-4 w-full text-white font-bold h-16 rounded-[10px]" onClick={fetchPaymentSheetParams}>
+            { loading ? <svg class="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24"></svg> : 'Confirm Order' }
+          </button>
+        }
       </div>
     </>
   );

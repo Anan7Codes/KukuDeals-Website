54bd71415a96bcb6b4c576343ff88221b6814858
Anan7Codes <anand07dhu@outlook.com>
2022-02-22 00:57:33 +0400

    id -> product_id

--- pages/api/payments/confirm-order.js
+++ pages/api/payments/confirm-order.js
@@ -110,14 +110,14 @@ const webhookHandler = async (req, res) => {
                 const campaign_for_qty = await supabase
                     .from('campaigns')
                     .select('SoldOutCoupons')
-                    .eq("id", item.id)
+                    .eq("id", item.product_id)
                     .single()
                 console.log("campaign for qty", campaign_for_qty)
                 if(campaign_for_qty.error) return res.send({ success: false, message: "Campaign Doesn't Exist", error: campaign_for_qty.error})
                 const campaign_update_qty = await supabase
                     .from('campaigns')
                     .update({ SoldOutCoupons: campaign_for_qty.data.SoldOutCoupons + item.product_qty })
-                    .eq("id", item.id)
+                    .eq("id", item.product_id)
                     console.log("campaign update qty", campaign_update_qty)
                 if(campaign_update_qty.error) return res.send({ success: false, message: "Campaign Qty Doesn't Exist", error: campaign_update_qty.error})
             })      

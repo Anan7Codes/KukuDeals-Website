9cf09cce601fe9e7aecb1bcd4dbe4cbd34f89940
Anan7Codes <anand07dhu@outlook.com>
2022-02-22 12:11:21 +0400

    reset password test

--- pages/reset-password.js
+++ pages/reset-password.js
@@ -9,8 +9,16 @@ export default function ResetPassword() {
   const [ password, setPassword ] = useState('');
   const [ confirmPassword, setConfirmPassword ] = useState('');
 
-  const ResetPassword = async (e) => {
-    e.preventDefault();
+  const ResetPassword = async () => {
+    toast.error('Simply', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined
+    });
     try {
         if(password !== confirmPassword) {
             return toast.error('Passwords do not match', {
@@ -54,40 +62,37 @@ export default function ResetPassword() {
   }
 
   return (
-    <div>
-      <div className="flex justify-center pt-20 pb-20">
-        <div className="w-1/2  rounded-[25px] bg-[#2c2c2c] mb-6 mt-10">
-          <div className="ml-28 pt-4">
-            <p className="text-3xl text-[#ffd601] font-bold">Reset Password</p>
-          </div>
-          <form
-            onSubmit={ResetPassword}
-            className="flex justify-center pb-6 pt-2"
-          >
-            <div className="flex flex-col">
-              <input
-                type="password"
-                className="border placeholder:text-xs text-xs pl-3 mr-3 w-full lg:w-96 mt-4 outline-none rounded-[5px] h-14 border-[#d3d3d3] bg-[#2c2c2c] text-white"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <input
-                type="password"
-                className="border placeholder:text-xs text-xs pl-3 mr-3 w-full lg:w-96 mt-4 outline-none rounded-[5px] h-14 border-[#d3d3d3] bg-[#2c2c2c] text-white"
-                placeholder="Confirm Password"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-              />
-              <div className="pb-6 flex justify-between">
-                <button className="bg-[#ffd601] mr-3 mt-4 w-full outline-none rounded-[5px] h-14 text-black font-semibold text-base">
-                    Reset Password
-                </button>
-              </div>
+    <div className="flex justify-center pt-20 pb-20 bg-[#161616]">
+        <div className="w-1/2 rounded-[25px] bg-[#2c2c2c] mb-6 mt-10">
+            <div className="ml-28 pt-4">
+                <p className="text-3xl text-[#ffd601] font-bold">Reset Password</p>
             </div>
-          </form>
+            <div
+                className="flex justify-center pb-6 pt-2"
+            >
+                <div className="flex flex-col">
+                    <input
+                        type="password"
+                        className="border placeholder:text-xs text-xs pl-3 mr-3 w-full lg:w-96 mt-4 outline-none rounded-[5px] h-14 border-[#d3d3d3] bg-[#2c2c2c] text-white"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        className="border placeholder:text-xs text-xs pl-3 mr-3 w-full lg:w-96 mt-4 outline-none rounded-[5px] h-14 border-[#d3d3d3] bg-[#2c2c2c] text-white"
+                        placeholder="Confirm Password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                    <div className="pb-6 flex justify-between">
+                        <button onClick={ResetPassword} className="bg-[#ffd601] mr-3 mt-4 w-full outline-none rounded-[5px] h-14 text-black font-semibold text-base">
+                            Reset Password
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
   );
 }

facd04b4084f731cf55f1489611d82c56beb583a
Anan7Codes <anand07dhu@outlook.com>
2022-02-23 15:19:40 +0400

    attemp 3

--- pages/api/payments/checkout-sessions.js
+++ pages/api/payments/checkout-sessions.js
@@ -19,37 +19,36 @@ export default async function handler(req, res) {
         return res.send({ success: false, message: 'Wrong request made'})
     }
     if (req.method === 'POST') {
-        try {
-        
-            if(req.body.user_id === '') return res.send({ success: false, message: "Unauthorized"})
+        try {        
+            // if(req.body.user_id === '') return res.send({ success: false, message: "Unauthorized"})
 
-            const { total, success } = await TotalPrice(req.body.cart)
-            if(!success) return res.status(404).json({ success: false, message: "Failed to calculate total amount"})
+            // const { total, success } = await TotalPrice(req.body.cart)
+            // if(!success) return res.status(404).json({ success: false, message: "Failed to calculate total amount"})
 
-            let finalTotal = total
-            if(req.body.promoCode) {
-                let promo_code = await supabase
-                    .from('promo_codes')
-                    .select('type,value')
-                    .eq("name", req.body.promoCode)
-                if(promo_code.data[0].type) {
-                    finalTotal = total - promo_code.data[0].value
-                } else {
-                    finalTotal = total - (total * promo_code.data[0].value / 100)
-                }
-                console.log(finalTotal)
-            }
+            // let finalTotal = total
+            // if(req.body.promoCode) {
+            //     let promo_code = await supabase
+            //         .from('promo_codes')
+            //         .select('type,value')
+            //         .eq("name", req.body.promoCode)
+            //     if(promo_code.data[0].type) {
+            //         finalTotal = total - promo_code.data[0].value
+            //     } else {
+            //         finalTotal = total - (total * promo_code.data[0].value / 100)
+            //     }
+            //     console.log(finalTotal)
+            // }
 
-            let { data, error } = await supabase
-                .from('profiles')
-                .select('stripe_customer_id')
-                .eq("id", req.body.user_id)
-            if(error) return res.status(404).json({ success: false, message: "Failed authorization"})
+            // let { data, error } = await supabase
+            //     .from('profiles')
+            //     .select('stripe_customer_id')
+            //     .eq("id", req.body.user_id)
+            // if(error) return res.status(404).json({ success: false, message: "Failed authorization"})
 
             const session = await stripe.checkout.sessions.create({
-                customer: data[0].stripe_customer_id,
+                customer: "cus_LC2S4YrghBk60u",
                 line_items: [
-                    {price: finalTotal.toFixed() * 100}
+                    {price: 10000}
                 ],
                 mode: 'payment',
                 success_url: `${req.headers.origin}/?success=true`,

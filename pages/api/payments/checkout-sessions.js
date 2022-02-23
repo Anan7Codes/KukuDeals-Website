7b6e87f0da929f82cae3b94698af662cb1a437ae
Anan7Codes <anand07dhu@outlook.com>
2022-02-23 16:32:57 +0400

    error fixes

--- pages/api/payments/checkout-sessions.js
+++ pages/api/payments/checkout-sessions.js
@@ -1,5 +1,6 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 import { map } from 'modern-async'
+import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseSecretKey = process.env.SUPABASE_SECRET_KEY

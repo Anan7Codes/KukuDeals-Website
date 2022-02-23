6bf96aee3b43c74e91eba3d0a57c0e4046f1e337
Anan7Codes <anand07dhu@outlook.com>
2022-02-23 16:36:38 +0400

    Response console

--- components/cart/Payment.js
+++ components/cart/Payment.js
@@ -83,6 +83,7 @@ export default function Payment() {
                 user_id: userInfo.id,
                 cart: cart
             })
+            console.log("response", response)
             if(!response.success) return alert(response.message)
             router.push(response.data.url)
             setLoading(false)

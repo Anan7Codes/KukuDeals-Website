e62f9dbafd14c53b073c8b20c533a61427d4074c
MohammedHafiz <mohammedhafizba@gmail.com>
2022-02-24 19:14:07 +0530

    test

--- pages/api/payments/confirm-order.js
+++ pages/api/payments/confirm-order.js
@@ -136,11 +136,9 @@ const webhookHandler = async (req, res) => {
             res_charge = charge
 
             const header = `
-                <!DOCTYPE html>
                 <html lang="en">
                 <head>
                     <meta charset="UTF-8">
-                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
                     <title>Tailwind CSS Invoce </title>
                     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">

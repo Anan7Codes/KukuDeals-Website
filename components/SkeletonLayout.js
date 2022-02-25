f2094ace2229861d950e9c861cad9bd6f699749e
Anan7Codes <anan07dhu@outlook.com>
2022-02-25 23:46:33 +0400

    and colour change

--- components/SkeletonLayout.js
+++ components/SkeletonLayout.js
@@ -5,7 +5,7 @@ import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 function SkeletonLayout({children}) {
     
     return (
-        <SkeletonTheme baseColor="#2c2c2c" highlightColor="#161616">
+        <SkeletonTheme baseColor="#1f1f1f" highlightColor="#161616">
             <div className="bg-[#161616]">
                 <div className="mx-auto max-w-[1266px] container px-5 pt-4 overflow-x-hidden min-h-screen">   
                     <Skeleton className="h-[55px] mb-4" style={{borderRadius: 15}} />

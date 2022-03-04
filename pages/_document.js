c351aa44fa7e311da09a074d7d8456af5f7cf186
MohammedHafiz <mohammedhafizba@gmail.com>
2022-03-04 14:37:44 +0530

    email testing'

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ class MyDocument extends Document {
                     <link rel="icon" href="icons/icon.png" />
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-                    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,400;0,500;0,700;1,100;1,500&family=Manrope:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet" />
+                    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,400;0,500;0,700;1,100;1,500&family=Manrope:wght@200;300;400;500;600;700;800&family=Roboto:ital,wght@0,100;0,300;1,100;1,300;1,500&display=swap" rel="stylesheet"/>
                 </Head>
                 <body>
                     <Main />

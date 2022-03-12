0a09f5aa92cef59be1298e97d98762189b2a6d0b
Anan7Codes <anan07dhu@outlook.com>
2022-03-12 13:24:21 +0400

    loading animation

--- components/SignUp.js
+++ components/SignUp.js
@@ -14,9 +14,10 @@ function SignUp() {
   const [gender, setGender] = useState(true);
   const [showNationality, setShowNationality] = useState(false);
   const [showCountryofResidence, setShowCountryOfResidence] = useState(false);
+  const [ loading, setLoading ] = useState(false);
 
   const SignUpUser = async (values) => {
-    console.log("values", values)
+    setLoading(true)
     try {
       if (values.password !== values.confirmPassword) {
         toast.error("Passwords does not match", {
@@ -28,6 +29,7 @@ function SignUp() {
           draggable: true,
           progress: undefined,
         });
+        setLoading(false)
         return;
       }
       const { user, error } = await supabase.auth.signUp(
@@ -47,7 +49,6 @@ function SignUp() {
           }
         }
       )
-      console.log("user", user)
       if (error) {
         toast.error(error, {
           position: "top-right",
@@ -58,6 +59,7 @@ function SignUp() {
           draggable: true,
           progress: undefined,
         });
+        setLoading(false)
         return
       }
       toast.success("Successfully signed up. You'll have to verify your email to continue", {
@@ -69,6 +71,7 @@ function SignUp() {
         draggable: true,
         progress: undefined,
       });
+      setLoading(false)
       router.push({pathname: '/phone-number-verification', query: { uid: user.id}})
       return
     }
@@ -290,7 +293,10 @@ function SignUp() {
                   Existing User? Click here
                 </p>
                 <button onClick={isValid ? handleSubmit : null} className="bg-[#ffd601]  mr-3 mt-4 w-full outline-none rounded-[5px] h-14 text-black font-semibold text-base">
-                  Sign Up
+                  { loading ? <svg role="status" className="mr-2 w-full h-8 animate-spin fill-black text-[#ffd601]" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                      <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                    </svg> : 'Sign Up'}
                 </button>
               </div>
             </div>

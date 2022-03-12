2ca6a8d2444c87f8187ac890a265b1bdba1397b4
Anan7Codes <anan07dhu@outlook.com>
2022-03-12 17:47:54 +0400

    phone verify

--- components/SignIn.js
+++ components/SignIn.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from 'next/router'
 import { toast } from "react-toastify";
 import { supabase } from '@/utils/supabaseClient';

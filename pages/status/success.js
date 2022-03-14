49b867fc6fd518361a17fae75069003b88d3a844
Anan7Codes <anan07dhu@outlook.com>
2022-03-14 12:16:50 +0400

    faq & pp only left

--- pages/status/success.js
+++ pages/status/success.js
@@ -84,7 +84,7 @@ function Success() {
     EmptyCart()
   }, []);
 
-  if(!success) {
+  if(success) {
     return (
     <div className={`bg-[#161616] overflow-x-hidden`} dir={locale === 'ar' ? 'rtl' : 'ltr'}>
       <Head>
